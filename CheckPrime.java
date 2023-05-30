353e581a8175d538f05536d8a44d2a41d06260bf
Jeevithak1 <134997314+Jeevithak1@users.noreply.github.com>
2023-05-30 11:35:27 +0530

    Add file

---
+++ CheckPrime.java
@@ -0,0 +1,22 @@
+class CheckPrime {
+    public static void main(String[] args) {
+        int n = 6, rem = 0, sum = 0, c = 0;
+        while (n > 0) {
+            rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
+        }
+        System.out.println(sum);
+        for (int i = 1; i <= sum; i++) {
+            if (sum % i == 0) {
+                c++;
+            }
+        }
+        if (c == 2) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not prime");
+        }
+    }
+
+}
